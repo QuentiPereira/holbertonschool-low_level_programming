b990fdfdc9f90c886298bba2eb5fda6391b40437
QuentiPereira <quenti.personal@gmail.com>
2024-02-16 16:20:02 +0000

    hola

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	int a;
 	long int b;
 	long long int c;
@@ -16,5 +17,5 @@ int main(void){
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);	
+	return (0);
 }
